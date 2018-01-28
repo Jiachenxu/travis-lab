d1e35a29c70fd4e462a8a554c39d46a10252a6a3
Jiachen Xu <jx2318@columbia.edu>
2018-01-28 13:40:11 -0500

    Update task222.py

--- task222.py
+++ task222.py
@@ -7,7 +7,8 @@
 def div(x,y):
     import numpy as np
     x = np.array(x).reshape(1,)
-    return np.true_divide(x,8)
+    y = np.array(y).reshape(1,)
+    return np.true_divide(x,y)
 
 def test_answer():
     assert div(2,8) != 0
